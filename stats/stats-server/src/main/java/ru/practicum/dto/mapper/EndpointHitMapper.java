84d128b64fa163b2b0157553b35edd0fc5a447a8
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-10-01 17:45:47 +0300

    feat: add stats-server dto mappers

---
+++ stats/stats-server/src/main/java/ru/practicum/dto/mapper/EndpointHitMapper.java
@@ -0,0 +1,11 @@
+package ru.practicum.dto.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import ru.practicum.ewm.stats.dto.EndpointHitDto;
+import ru.practicum.model.EndpointHit;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface EndpointHitMapper {
+    EndpointHit endpointHitDtoToEndpointHit(EndpointHitDto endpointHitDto);
+}
