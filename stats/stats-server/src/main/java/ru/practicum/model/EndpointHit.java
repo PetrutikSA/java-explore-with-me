630bc0ac625c2143f3a1d9466c518e8755bb04fe
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-10-01 17:17:40 +0300

    feat: add stats-server scheme.sql

--- stats/stats-server/src/main/java/ru/practicum/model/EndpointHit.java
+++ stats/stats-server/src/main/java/ru/practicum/model/EndpointHit.java
@@ -1,21 +1,32 @@
 package ru.practicum.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.SecondaryTable;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.time.Instant;
 
 @Entity
+@Table(name = "endpoints_hits")
+@SecondaryTable(name = "apps", pkJoinColumns = @PrimaryKeyJoinColumn(name = "app_id"))
+@SecondaryTable(name = "uris", pkJoinColumns = @PrimaryKeyJoinColumn(name = "uri_id"))
+@SecondaryTable(name = "ips", pkJoinColumns = @PrimaryKeyJoinColumn(name = "ip_id"))
 @Data
 public class EndpointHit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name = "name", table = "apps")
     private String app;
+    @Column(name = "name", table = "uris")
     private String uri;
+    @Column(name = "name", table = "ips")
     private String ip;
     private Instant timestamp;
 }
