bde234a088184accb786da359c87edb91faf8ca7
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-10-01 14:30:12 +0300

    feat: add stats-server StatsRepository JPA

--- stats/stats-server/src/main/java/ru/practicum/service/StatsServiceBase.java
+++ stats/stats-server/src/main/java/ru/practicum/service/StatsServiceBase.java
@@ -1,13 +1,17 @@
 package ru.practicum.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.practicum.ewm.stats.dto.EndpointHitDto;
 import ru.practicum.ewm.stats.dto.ViewStatsDto;
+import ru.practicum.repository.StatsRepository;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class StatsServiceBase implements StatsService {
+    private final StatsRepository statsRepository;
 
     @Override
     public void createRecord(EndpointHitDto endpointHitDto) {
