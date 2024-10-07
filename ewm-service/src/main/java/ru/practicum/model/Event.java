d0ad0c3b1fe29bccedd36aa79b5c79ed9739394f
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-10-07 15:57:06 +0300

    feat: main service add entities annotations

--- ewm-service/src/main/java/ru/practicum/model/Event.java
+++ ewm-service/src/main/java/ru/practicum/model/Event.java
@@ -1,23 +1,51 @@
 package ru.practicum.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import ru.practicum.model.enums.EventState;
 
 import java.time.Instant;
 
+@Entity
+@Table(name = "locations")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String annotation;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
     private int confirmedRequests;
     private Instant createdOn;
     private String description;
     private Instant eventDate;
+    @ManyToOne
+    @JoinColumn(name = "initiator_id")
     private User initiator;
+    @ManyToOne
+    @JoinColumn(name = "location_id")
     private Location location;
     private boolean paid;
     private int participantLimit;
     private Instant publishedOn;
     private boolean requestModeration;
+    @Enumerated(EnumType.STRING)
     private EventState state;
     private String title;
     private long views;
