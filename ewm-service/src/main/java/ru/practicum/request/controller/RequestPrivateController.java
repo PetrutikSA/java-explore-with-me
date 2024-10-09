7bdea8cb1e5210da53df5a7afa9b07e4981c9841
Stanislav Petrutik <petrutiksa@yandex.ru>
2024-10-09 22:09:40 +0300

    feat: add Request private service

--- ewm-service/src/main/java/ru/practicum/request/controller/RequestPrivateController.java
+++ ewm-service/src/main/java/ru/practicum/request/controller/RequestPrivateController.java
@@ -1,5 +1,6 @@
 package ru.practicum.request.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -10,30 +11,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.dto.event.request.ParticipationRequestDto;
+import ru.practicum.request.service.RequestPrivateService;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/users/{userId}/requests")
+@RequiredArgsConstructor
 public class RequestPrivateController {
+    private final RequestPrivateService requestPrivateService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ParticipationRequestDto createParticipationRequest(@PathVariable Long userId,
                                                               @RequestParam Long eventId) {
-        return null;
+        return requestPrivateService.createParticipationRequest(userId, eventId);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ParticipationRequestDto> getAllOwnParticipationRequests(@PathVariable Long userId) {
-        return null;
+        return requestPrivateService.getAllOwnParticipationRequests(userId);
     }
 
     @PatchMapping("/{requestId}/cancel")
     @ResponseStatus(HttpStatus.OK)
     public ParticipationRequestDto cancelOwnParticipationInEvent(@PathVariable Long userId,
                                                                  @PathVariable Long requestId) {
-        return null;
+        return requestPrivateService.cancelOwnParticipationInEvent(userId, requestId);
     }
 }
